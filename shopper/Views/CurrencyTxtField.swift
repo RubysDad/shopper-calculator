34491342c9711b088e5ebbe5931d35c7fdbaa7b1
Mark Morales <marktravismorales@gmail.com>
2018-12-21 14:11:25 -0600

    Added Currency symbol on currencyTxtField

--- shopper/Views/CurrencyTxtField.swift
+++ shopper/Views/CurrencyTxtField.swift
@@ -10,6 +10,23 @@ import UIKit
 
 @IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.6814449112, green: 0.7378152968, blue: 0.8197074649, alpha: 0.8028413955)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        // Adding sub views to a view
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -23,6 +40,8 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.254093536)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
